a802302eef76e38eb8ccd818f284c36171c57cad
wudye <mingwei.wu@hotmail.com>
2026-05-21 18:38:21 +0200

    21

--- backend/app/gateway/app.py
+++ backend/app/gateway/app.py
@@ -5,6 +5,18 @@ import asyncio
 import logging
 from collections.abc import AsyncGenerator
 from fastapi.middleware.cors import CORSMiddleware
+from spark.config import app_config as spark_app_config
+from spark.config.app_config import apply_logging_level
+from contextlib import asynccontextmanager
+
+
+
+get_app_config = spark_app_config.get_app_config
+
+
+
+
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,10 +26,29 @@ logging.basicConfig(
 
 logger= logging.getLogger(__name__)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    get_app_config()
+
+    try:
+        print("test and delete later in lifespan")
+        yield
+    finally:
+        print("test and delete later in lifespan")
+
+
 
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(
+        lifespan=lifespan
+    )
+    
+
+    @app.get("/")
+    async def root():
+        return {"message": "Hello World"}
+    
     return app
 
-app = create_app
+app: Callable[[], FastAPI] = create_app()
 
